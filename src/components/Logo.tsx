74133e398e96062bdd8f51b9396d40d1c1e0af52
zdenham <zdenham1@gmail.com>
2023-11-21 17:04:39 -0800

    add favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Logo = () => {
   return (
     <div>
-      <Image src="/logo.png" alt="Logo" width={50} height={50} />
+      <Image src="/logoSmall.png" alt="Logo" width={50} height={50} />
     </div>
   );
 };
