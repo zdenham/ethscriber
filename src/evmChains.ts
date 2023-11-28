6a816bf7819bcca11ceb636840a72ff6e3f64d1d
zdenham <zdenham1@gmail.com>
2023-11-27 19:56:54 -0800

    add op bnb mainnet

--- src/evmChains.ts
+++ src/evmChains.ts
@@ -9,9 +9,36 @@ import {
   canto,
   celo,
   fantom,
+  Chain,
 } from 'wagmi/chains';
 
+const opBNBMainnet: Chain = {
+  id: 204,
+  network: 'opBNB',
+  name: 'opBNB Mainnet',
+  nativeCurrency: {
+    name: 'Binance Coin',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://1rpc.io/opbnb'],
+    },
+    public: {
+      http: ['https://1rpc.io/opbnb'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'opBNBScan',
+      url: 'https://opbnbscan.com',
+    },
+  },
+};
+
 export const evmChains = [
+  opBNBMainnet,
   mainnet,
   arbitrum,
   avalanche,
@@ -23,3 +50,6 @@ export const evmChains = [
   celo,
   fantom,
 ];
+
+
+
